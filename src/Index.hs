af4f4decb40174f1aaa3d598a960511ebe3c147c
Gideon Providence <jprovidence@live.ca>
2011-12-07 14:12:09 -0500

    first commit

---
+++ src/Index.hs
@@ -0,0 +1,137 @@
+module Index (
+    IndexTree(IndexTree)
+,   IndexPrototype(IndexPrototype)
+,   pushIndex
+,   indexLookup
+) where
+
+import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Internal as BI
+import Data.Binary.Put
+import Data.Word
+import Data.List as L
+import Data.Maybe
+import System.IO
+import System.IO.Unsafe
+import Foreign.Marshal.Alloc
+
+
+type ByteString = B.ByteString
+
+type NodeIndex = Word32
+
+type Node = Word32
+
+data IndexTpl = Tpl Word8 (Maybe Word32) (Maybe Word32)
+
+data IndexTree = IndexTree ByteString [IndexTree] (Maybe NodeIndex)
+
+data IndexPrototype = IndexPrototype ByteString NodeIndex
+
+
+maxFilePos = 4294967295
+
+stdIdxPath = "/home/providence/Dropbox/_ticket/haskell_devel/fiber/fiber/data/index_initial._ticket"
+
+
+----------------------------------------------------------------------------------------------------
+
+-- STANDARD DISK INDEX --
+
+----------------------------------------------------------------------------------------------------
+
+pushStdIdx :: IndexPrototype -> ()
+pushStdIdx (IndexPrototype bstr ni)
+    | (B.null bstr) /= True && ((L.head "h") == (B.head bstr)) == True = unsafePerformIO $ do
+        h <- openFile stdIdxPath ReadWriteMode
+        hSetBinaryMode h True
+        _ <- updateIndexGiven (IndexPrototype bstr ni) h 0
+        hClose h
+    | otherwise = unsafePerformIO $ do
+        putStrLn "_error_psi: Non-standard URL, ignoring..."
+
+
+updateIndexGiven :: IndexPrototype -> Handle -> Int -> IO ()
+updateIndexGiven (IndexPrototype bstr ni) h pos = do
+    ptr <- mallocBytes 4
+    count <- hGetBuf h ptr 4
+    case count == 4 of
+        True ->
+        False -> putStrLn "_error_ugi: File IO error, ignoring..."
+
+
+
+
+----------------------------------------------------------------------------------------------------
+
+-- FLOATING INDEX --
+
+----------------------------------------------------------------------------------------------------
+
+-- delegates floating index lookup to #iLookup providing default parameters. This removes the need
+-- to remember to supply these each time
+
+indexLookup :: IndexTree -> ByteString -> (Maybe NodeIndex)
+indexLookup tree str = iLookup tree str 0
+
+
+----------------------------------------------------------------------------------------------------
+
+-- loads IndexTrees recursively based on the ByteString argument. Either
+    -- A : The ByteString will unravel, and the IndexTree reached will contain the NodeIndex
+    -- B : The ByteString will unravel, and the IndexTree reached will not contain the NodeIndex
+    -- C : The ByteString will specify a non-existent path through the IndexTree, in which case
+    --     Nothing will be the value of the func
+
+iLookup :: IndexTree -> ByteString -> Int -> (Maybe NodeIndex)
+iLookup (IndexTree tbyte ttree tni) str pos
+    | (B.length str) == pos = tni
+    | otherwise = case (B.index str pos) `B.elem` tbyte of
+                      True -> iLookup (ttree !! (elemIdx str tbyte pos)) str (pos + 1)
+                      False -> Nothing
+
+
+----------------------------------------------------------------------------------------------------
+
+-- Delegates insertion into a floating index to #push, specifing default arguments
+
+pushIndex :: IndexTree -> IndexPrototype -> IndexTree
+pushIndex tree proto = push tree proto 0
+
+
+----------------------------------------------------------------------------------------------------
+
+-- Recursively traverses the IndexTree based on the IndexPrototype ByteString to insert the
+-- IndexPrototype value. When the path differs from existing paths, new IndexTree 'levels' are
+-- created and linked into the existing structure
+
+push :: IndexTree -> IndexPrototype -> Int -> IndexTree
+push (IndexTree tbyte ttree tni) (IndexPrototype pbyte pni) idx
+    | (B.length pbyte) == idx = (IndexTree tbyte ttree (Just pni))
+    | otherwise = case (B.index pbyte idx) `B.elem` tbyte of
+                      True -> (IndexTree tbyte (replace ttree
+                                                         (elemIdx pbyte tbyte idx)
+                                                         (push (ttree !! (elemIdx pbyte tbyte idx))
+                                                               (IndexPrototype pbyte pni)
+                                                               (idx + 1)))
+                                          tni)
+                      False -> (IndexTree (tbyte `B.snoc` (B.index pbyte idx))
+                                           (ttree ++ [(push (IndexTree B.empty [] Nothing)
+                                                           (IndexPrototype pbyte pni)
+                                                           (idx + 1))])
+                                           tni)
+
+    where replace :: [IndexTree] -> Int -> IndexTree -> [IndexTree]
+          replace lst idx other = let (x, _:ys) = L.splitAt idx lst
+                                  in x ++ other : ys
+
+
+----------------------------------------------------------------------------------------------------
+
+-- Given ByteString a & b and int i, this func finds the elem at index i in ByteString a, then
+-- returns the index of this element in ByteString b
+
+elemIdx :: ByteString -> ByteString -> Int -> Int
+elemIdx pbyte tbyte idx = fromJust $ B.elemIndex (B.index pbyte idx) tbyte
+
---
+++ src/Main.hs
@@ -0,0 +1,26 @@
+module Main (
+    main
+) where
+
+import Index
+import qualified Data.ByteString.Char8 as B
+
+main = do
+    bool <- testIndexPush
+    if bool == True
+        then putStrLn "_test: Index Lookup Successful"
+        else putStrLn "_test: _ERR_ Index Lookup Unsuccessful"
+
+
+testProto1 = IndexPrototype (B.pack "www.test1.com/test/1") 50
+testProto2 = IndexPrototype (B.pack "www.test2.com/other/2") 100
+
+testIndex = IndexTree B.empty [] Nothing
+
+testIndexPush :: IO (Bool)
+testIndexPush =
+    let postPush1 = pushIndex testIndex testProto1
+        postPush2 = pushIndex postPush1 testProto2
+    in case indexLookup postPush2 (B.pack "www.test2.com/other/2") of
+           Just _ -> return True
+           Nothing -> return False
