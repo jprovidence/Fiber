765d2aa22f72ff18a8a1a7bf3e9ea2ea5d526726
Gideon Providence <jprovidence@live.ca>
2011-12-08 06:42:24 -0500

    update index + some docu

--- src/Index.hs
+++ src/Index.hs
@@ -14,6 +14,7 @@ import Data.List as L
 import Data.Maybe
 import System.IO
 import System.IO.Unsafe
+import Control.Monad
 import Foreign.Marshal.Alloc
 import Foreign.Storable
 
@@ -30,6 +31,10 @@ data IndexTree = IndexTree ByteString [IndexTree] (Maybe NodeIndex)
 
 data IndexPrototype = IndexPrototype ByteString NodeIndex
 
+data Ternary a = ReadOnly a
+                | Vertical a
+                | Horizontal a
+
 
 maxFilePos = 4294967295
 
@@ -42,53 +47,128 @@ stdIdxPath = "/home/providence/Dropbox/_ticket/haskell_devel/fiber/fiber/data/in
 
 ----------------------------------------------------------------------------------------------------
 
+-- Handles much the cruft of opening/closing + setting binary mode on the file
+-- Delegates the updating of the file given the list of index prototypes to #updateIndexGiven
+
 pushStdIdx :: IndexPrototype -> ()
 pushStdIdx (IndexPrototype bstr ni)
     | (B.null bstr) /= True && ((L.head "h") == (B.head bstr)) == True = unsafePerformIO $ do
         h <- openFile stdIdxPath ReadWriteMode
         hSetBinaryMode h True
-        _ <- updateIndexGiven (IndexPrototype bstr ni) h 0 (Just 0)
+        _ <- updateIndexGiven (IndexPrototype bstr ni) h 0 (ReadOnly 0)
         hClose h
     | otherwise = unsafePerformIO $ do
         putStrLn "_error_psi: Non-standard URL, ignoring..."
 
 
-updateIndexGiven :: IndexPrototype -> Handle -> Int -> Maybe Int -> IO ()
+----------------------------------------------------------------------------------------------------
 
-updateIndexGiven (IndexPrototype bstr ni) h strPos (Just filePos) = do
+-- This function takes an IndexPrototype and commits it to the actual index store, stored on a flat
+-- file. Given the fig 1. & fig 2, this function works as follows
+
+--   Indexing the urls "www.google.com", "www.hoogle.com" and "www.hammer.com"
+--
+--                                           w           --
+--                                           |             |- each of these is an 'index level'
+--                                           w           --
+--                                           |
+--                                           w
+--                                           |
+--                                           .
+--                                           |
+--                                           g-->h
+--                                           |   |
+--                                           o   o-->a   -- an 'index level' indexing multiple
+--                                           .   .   |      characters
+--                                           .   .   m
+--                                           .   .   .
+--                                           |   |   .
+--                                           o   o   .
+--                                           |   |   |
+--                                           m   m   m
+--                                           |   |   |
+--                                           $   $   $
+--                                           |   |   |
+--                                      (Node)(Node)(Node)
+--
+--   This is stored on the files in 'IndexTriples' which consist of a grouping Char Word32a Word32b
+--   Char,    the character indexed at this position
+--   Word32a, the file position of the beginning of the next index level
+--   Word32b, the file position of the next char indexed at this index level
+--
+--   As such, the beginning of this index on disk word look like:
+--
+--  ['w', 10, 1] ['w', 19, 1] ['w', 28, 1] ['.', 0, 1] ['g', 0, 1] ['o', 0, 1] ['o', 0, 1] ['g', 0, 1]
+--    1   2   6   10   11  15   19  20 24  28   29 33   37  38 42   46  47 51   55  56 60  64   65 69
+
+--  ['l', 0, 1] ['e', 0, 1] ['.', 0, 1] ['c', 0, 1] ['o', 0, 1] ['m', 0, 1]
+
+
+updateIndexGiven :: IndexPrototype -> Handle -> Int -> Ternary Int -> IO ()
+
+updateIndexGiven (IndexPrototype bstr ni) h strPos (ReadOnly filePos) =
     updateIndexGiven (IndexPrototype bstr ni) h (strPos + 1)
                      (seekToDollar h (toInteger filePos) (B.index bstr strPos))
 
+updateIndexGiven (IndexPrototype bstr ni) h strPos (Horizontal filePos) = do
+    writePos <- startCleanBlock h (B.index bstr strPos)
+    updateIndexGiven (IndexPrototype bstr ni) h (strPos + 1) (Vertical $ backtrack 4 h)
+    linkToPrevious h filePos writePos
 
-seekToDollar :: Handle -> Integer -> Char -> Maybe Int
+updateIndexGiven (IndexPrototype bstr ni) h strPos (Vertical filePos) = do
+    writePos <- startCleanBlock h (B.index bstr strPos)
+    updateIndexGiven (IndexPrototype bstr ni) h (strPos + 1) (Vertical $ backtrack 8 h)
+    linkToPrevious h filePos writePos
+
+startCleanBlock :: Handle -> Char -> IO (Int)
+startCleanBlock h cha = do
+    hSeek h SeekFromEnd 0
+    writePos <- liftM fromInteger (hTell h)
+    writeBytes 1 h cha
+    writeBytes 4 h (0 :: Int)
+    writeBytes 4 h (1 :: Int)
+    return $ writePos
+
+linkToPrevious :: Handle -> Int -> Int -> IO ()
+linkToPrevious h filePos writePos = do
+    hSeek h AbsoluteSeek (toInteger filePos)
+    writeBytes 4 h (writePos :: Int)
+
+
+writeBytes :: (Storable a) => Int -> Handle -> a -> IO ()
+writeBytes nBytes h obj = do
+    ptr <- mallocBytes nBytes
+    poke ptr obj
+    hPutBuf h ptr nBytes
+    free ptr
+
+
+readBytes :: (Storable a) => Int -> Handle -> a
+readBytes nBytes h = unsafePerformIO $ do
+    ptr <- mallocBytes nBytes
+    hGetBuf h ptr nBytes
+    ret <- peek ptr
+    free ptr
+    return $ ret
+
+
+backtrack :: Int -> Handle -> Int
+backtrack i h = (\x -> x - i) . fromInteger . unsafePerformIO $ hTell h
+
+
+seekToDollar :: Handle -> Integer -> Char -> Ternary Int
 seekToDollar h fpos cha
-    | fpos == 1 = Nothing
+    | fpos == 1 = Horizontal ((fromInteger $ unsafePerformIO $ hTell h) - 4)
     | otherwise = unsafePerformIO $ do
-        ptr <- mallocBytes 1
         hSeek h AbsoluteSeek fpos
-        _ <- hGetBuf h ptr 1
-        derefd <- peek ptr
+        derefd <- return $ readBytes 1 h
         case derefd == cha of
             True -> do
-                free ptr
-                ptr <- mallocBytes 4
-                _ <- hGetBuf h ptr 4
-                ret <- peek ptr
-                free ptr
-                return $ Just ret
+                return $ ReadOnly (readBytes 4 h)
             False -> do
-                free ptr
-                ptr <- mallocBytes 4
                 hSeek h RelativeSeek 4
-                _ <- hGetBuf h ptr 4
-                ret <- peek ptr :: IO (Int)
-                free ptr
-                return $ seekToDollar h (toInteger ret) cha
-
-
-
-
-
+                newPos <- return $ (readBytes 4 h :: Int)
+                return $ seekToDollar h (toInteger newPos) cha
 
 
 
